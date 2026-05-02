f6517a39036ec36198154a7cde9cf2f86d41d9ee
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2026-05-03 00:00:34 +0300

    DEV docker compose set up for PG with replicas

--- docker/primary/migrations/001_init.up.sql
+++ docker/primary/migrations/001_init.up.sql
@@ -1,9 +1,3 @@
-CREATE USER replicator REPLICATION LOGIN PASSWORD 'replica_pass';
-
-ALTER SYSTEM SET wal_level = replica;
-ALTER SYSTEM SET max_wal_senders = 10;
-ALTER SYSTEM SET hot_standby = on;
-
 CREATE TABLE IF NOT EXISTS users (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     first_name TEXT,
